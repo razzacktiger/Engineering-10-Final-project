06aef0e839d515d0b5c8fe79d392022232dbdec4
harra <harra@192.168.131.120>
2021-05-16 23:59:16 -0700

    I added the basic Window frame code

--- ENG Project Game/src/Game1/PingPMain.java
+++ ENG Project Game/src/Game1/PingPMain.java
@@ -4,9 +4,21 @@ public class PingPMain extends Canvas implements Runnable{
 	public static void main(String [] args) {
 	
 	}
-	public PingPMain() {
-		
+	public static final int WIDTH = 1000;
+	public static final int HEIGHT = WIDTH * 9/16;
+	
+	public boolean running = false;
+	private Thread gameThread;
+	
+	private Ball ball;
+	private Paddle paddle1;
+	private Paddle paddle2; 
+	
+	public static void main(String [] args) {
+	
 	}
+	public PingPMain() {
+		canvasSetup();
 	public void run() {
 		
 	}
--- ENG Project Game/src/Game1/Window.java
+++ ENG Project Game/src/Game1/Window.java
@@ -5,9 +5,21 @@ public class Window {
 	public Window(JFrame title, PingPMain game) 
 	{ 
 	JFrame frame = new JFrame();
+	// creates object of Window Jframe 
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	// set up the close operation
 	frame.setResizable(false);
+	//The user cannot resize the window
 	frame.add(game);
+	//putting the running app in the frame
+	frame.pack();
+	// pack all of the components with a "perfect fit"
+	frame.setLocationRelativeTo(null)
+	// center the game to the screen
+	frame.setVisible(True);
+	// sets the window frame as visible
 	
+	game.start();
+	//start the game
 	}
 }

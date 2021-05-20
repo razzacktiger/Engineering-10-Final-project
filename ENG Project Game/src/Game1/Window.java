2f49cae2fa0a8d5a6f3b572b80f504abfc70f3d1
harra <harra@192.168.131.120>
2021-05-19 21:29:05 -0700

    Added the Modified Pong Game with 2 balls

--- ENG Project Game/src/Game1/Window.java
+++ ENG Project Game/src/Game1/Window.java
@@ -1,34 +1,31 @@
 package Game1;
 
-import java.awt.Color;
 import javax.swing.JFrame;
-import javax.swing.ImageIcon;
-import javax.swing.JColorChooser;
 
+
+/**
+ * window class
+ * 
+ * @author Zayed
+ *
+ */
 public class Window {
-	public Window(String title, PingPMain game) 
-	{ 
-	JFrame frame = new JFrame();
-	// creates object of Window Jframe 
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	// set up the close operation
-	frame.setResizable(false);
-	//The user cannot resize the window
-	frame.add(game);
-	//putting the running app in the frame
-	frame.pack();
-	// pack all of the components with a "perfect fit"
-	frame.setLocationRelativeTo(null);
-	// center the game to the screen
-	frame.setVisible(true);
-	// sets the window frame as visible
-	
-	//frame.getContentPane().setBackground(new Color(30, 54, 200));
-	//sets the backround color to bluish green
-	
-	frame.setTitle("JFrame is Da Boss"); 
-	// sets title of frame
-	game.start();
-	//start the game
+
+	/**
+	 * Create the frame.
+	 * 
+	 * @param title - desired title of the game
+	 * @param game  - the game
+	 */
+	public Window(String title, Game game) {
+		JFrame frame = new JFrame(title);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.add(game); // Game inherits from Canvas, a Component object, so it can be put in a JFrame
+		frame.pack();
+		frame.setLocationRelativeTo(null); // ghetto way of centering the window
+		frame.setVisible(true);
 	}
+
 }
